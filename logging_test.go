6cd6b387ea3261cfcf113adb072a7e2e9022a5f1
Patrick <devnull@mailinator.com>
2017-07-20 14:28:28 +0800

    add more tests

--- logging_test.go
+++ logging_test.go
@@ -9,43 +9,75 @@ import (
 
 func TestLogging(t *testing.T) {
 	cfg := Config{
+		File:   "/tmp/a",
+		Debug:  false,
+		Prefix: "TST",
+	}
+	Init(cfg)
+	defer os.Remove(cfg.File)
+
+	P("this p-test should be in `%s'", cfg.File)
+	D("AIYO d-test should not appear in `%s'", cfg.File)
+
+	txt, err := ioutil.ReadFile(cfg.File)
+	if err != nil {
+		t.Errorf("failed to read `%s': %v", cfg.File, err)
+	}
+	s := string(txt)
+
+	if strings.Contains(s, "WARN") == false {
+		t.Errorf("logfile `%s' does not WARN prefix", cfg.File)
+	}
+	if strings.Contains(s, "AIYO") == true {
+		t.Errorf("logfile `%s' contains debug when debug not set", cfg.File)
+	}
+}
+
+func TestDebug(t *testing.T) {
+	cfg := Config{
+		File:   "/tmp/a",
+		Debug:  true,
+		Prefix: "TST",
+	}
+	Init(cfg)
+	defer os.Remove(cfg.File)
+
+	P("this p-test should be in `%s'", cfg.File)
+	D("AIYO d-test should appear in `%s'", cfg.File)
+
+	txt, err := ioutil.ReadFile(cfg.File)
+	if err != nil {
+		t.Errorf("failed to read `%s': %v", cfg.File, err)
+	}
+	s := string(txt)
+
+	if strings.Contains(s, "WARN") == false {
+		t.Errorf("logfile `%s' does not WARN prefix", cfg.File)
+	}
+	if strings.Contains(s, "AIYO") == false {
+		t.Errorf("logfile `%s' does not contain debug when debug set", cfg.File)
+	}
+}
+
+func TestAuditLogging(t *testing.T) {
+	cfg := Config{
 		File:      "/tmp/a",
 		Debug:     false,
 		AuditFile: "/tmp/b",
 		Prefix:    "TST",
 	}
 	Init(cfg)
-	P("this p-test should be in `%s'", cfg.File)
-	D("XXX d-test should not appear in `%s'", cfg.File)
-
-	if txt, err := ioutil.ReadFile(cfg.File); err == nil {
-		s := string(txt)
-
-		if strings.Contains(s, "WARN") == false {
-			t.Errorf("logfile `%s' does not WARN prefix", cfg.File)
-		}
-		if strings.Contains(s, "XXX") == true {
-			t.Errorf("logfile `%s' contains debug when debug not set", cfg.File)
-		}
-		println(s)
-	} else {
-		t.Errorf("failed to read `%s': %v", cfg.File, err)
-	}
-	os.Remove(cfg.File)
-
-	if cfg.AuditFile == "" {
-		return
-	}
+	defer os.Remove(cfg.File)
+	defer os.Remove(cfg.AuditFile)
 
 	A("a-test did some testing for `%s'", cfg.AuditFile)
-	if txt, err := ioutil.ReadFile(cfg.AuditFile); err == nil {
-		s := string(txt)
-
-		if strings.Contains(s, "testing") == false {
-			t.Errorf("logfile `%s' does not testing line", cfg.AuditFile)
-		}
-	} else {
+	txt, err := ioutil.ReadFile(cfg.AuditFile)
+	if err != nil {
 		t.Errorf("failed to read `%s': %v", cfg.AuditFile, err)
 	}
-	os.Remove(cfg.AuditFile)
+
+	s := string(txt)
+	if strings.Contains(s, "testing") == false {
+		t.Errorf("logfile `%s' does not testing line", cfg.AuditFile)
+	}
 }
